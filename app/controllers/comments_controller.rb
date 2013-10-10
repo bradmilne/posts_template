5d4979b582f4befe4d03b1ef1dd30a2cb61f6af7
Brad Milne <brad.milne@getelevate.com>
2013-10-10 10:50:42 -0400

    change comment issue

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+
+
+  def create
+  	@post = Post.find(params[:post_id])
+  	@comment = @post.comments.new(comment_params)
+  	@comment.user_id = 1
+  	if @comment.save
+  		flash[:notice] = "Your comment was created"
+  		redirect_to post_path(@post)
+  	else
+  		render 'posts/show'
+  	end
+  end
+
+  def comment_params
+  	params.require(:comment).permit(:body)
+  end
+
+end
